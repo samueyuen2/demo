adb5f6704ee83f9b70a550c6b4533d624561758a
samueyuen2 <58627143+samueyuen2@users.noreply.github.com>
2024-04-23 23:51:35 +0800

    Update frontend

--- frontend/src/api/axiosWrapper.jsx
+++ frontend/src/api/axiosWrapper.jsx
@@ -5,7 +5,7 @@ const initAxios = axios.create();
 export default async function axiosWrapper(method, url, data) {
     return initAxios({
         method: method,
-        url: url,
+        url: "https://demo-backend-kappa.vercel.app" + url,
         responseType: 'json',
         data: data
     });
